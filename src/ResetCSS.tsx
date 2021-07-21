3b55b32d6f0661415fa62311a3b2af9e913ce1ca
Thanakait Homhual <61160061@go.buu.ac.th>
2021-07-22 01:53:40 +0700

    edit scoll bar color

--- src/ResetCSS.tsx
+++ src/ResetCSS.tsx
@@ -84,11 +84,11 @@ const ResetCSS = createGlobalStyle`
     width: 8px;
   }
   ::-webkit-scrollbar-thumb {
-    background: ${({ theme }) => theme.colors.textSubtle}; 
+    background: ${({ theme }) => theme.colors.bitkub}; 
     border-radius: 8px;
   }
   ::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 5px ${({ theme }) => theme.colors.input}; 
+    box-shadow: inset 0 0 5px ${({ theme }) => theme.colors.bitkub}; 
     border-radius: 10px;
   }
 
