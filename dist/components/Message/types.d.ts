f76f8e420bcbd57afa80fb73dcddb3cae98fb8a7
Thanakait Homhual <61160061@go.buu.ac.th>
2021-07-23 02:58:35 +0700

    change logo bitkub

--- dist/components/Message/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { SpaceProps } from "styled-system";
-export declare const variants: {
-    readonly WARNING: "warning";
-    readonly DANGER: "danger";
-};
-export declare type Variant = typeof variants[keyof typeof variants];
-export interface MessageProps extends SpaceProps {
-    variant: Variant;
-}
