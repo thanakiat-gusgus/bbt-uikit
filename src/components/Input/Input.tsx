1a8ff0e62f0bdf1da127034ec8def8ba35ad6c65
wongsatornnurse <70052037+wongsatornnurse@users.noreply.github.com>
2021-07-27 18:57:17 +0700

    change color input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -17,7 +17,7 @@ const getBoxShadow = ({ isSuccess = false, isWarning = false, theme }: StyledInp
     return theme.shadows.success;
   }
 
-  return theme.shadows.inset;
+  return theme.shadows.bbtColor;
 };
 
 const getHeight = ({ scale = scales.MD }: StyledInputProps) => {
@@ -58,7 +58,7 @@ const Input = styled.input<InputProps>`
   }
 
   &:focus:not(:disabled) {
-    box-shadow: ${({ theme }) => theme.shadows.bbtColor};
+    box-shadow: ${({ theme }) => theme.shadows.inset};
   }
 `;
 
