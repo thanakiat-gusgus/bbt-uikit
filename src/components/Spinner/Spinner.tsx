6d274465aac2b05eec56dd40a97c864a5dd17ac4
book.siras <book.siras@bitkub.com>
2021-07-30 13:06:45 +0700

    change spinner icon to BitkubIcon

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 import PanIcon from "./PanIcon";
 import PancakeIcon from "./PancakeIcon";
+import BitkubIcon from "./BitkubIcon";
 import { SpinnerProps } from "./types";
 
 const rotate = keyframes`
@@ -42,11 +43,16 @@ const FloatingPanIcon = styled(PanIcon)`
   transform: translate3d(0, 0, 0);
 `;
 
+//BitkubIcon
+const FloatingBitkubIcon = styled(BitkubIcon)`
+  animation: ${float} 6s ease-in-out infinite;
+  transform: translate3d(0, 0, 0);
+`;
+
 const Spinner: React.FC<SpinnerProps> = ({ size = 128 }) => {
   return (
     <Container>
-      <RotatingPancakeIcon width={`${size * 0.5}px`} />
-      <FloatingPanIcon width={`${size}px`} />
+      <FloatingBitkubIcon width={`${size}px`} />
     </Container>
   );
 };
