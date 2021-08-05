b257bc3d084046a73eb7414f60d256c0d5763734
wongsatornnurse <70052037+wongsatornnurse@users.noreply.github.com>
2021-08-05 11:38:24 +0700

    edit color and Icon in Breadcrumb

--- src/components/Breadcrumbs/index.stories.tsx
+++ src/components/Breadcrumbs/index.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Text from "../Text/Text";
 import Link from "../Link/Link";
 import LogoIcon from "../Svg/Icons/Logo";
-import PresentWonIcon from "../Svg/Icons/PresentWon";
+import KubIcon from "../Svg/Icons/KubIcon";
 import BreadCrumbs from "./Breadcrumbs";
 
 export default {
@@ -15,15 +15,15 @@ export const Default: React.FC = () => {
   return (
     <Text p="32px">
       <BreadCrumbs mb="32px">
-        <Link href="/" color="secondary" style={{ fontWeight: 400 }}>
+        <Link href="/" color="bbtColor" style={{ fontWeight: 400 }}>
           Link
         </Link>
         <Text color="textDisabled">Crumb 1</Text>
         <Text color="textDisabled">Crumb 2</Text>
       </BreadCrumbs>
       <BreadCrumbs>
-        <Text>PancakeSwap</Text>
-        <Text>The #1 AMM and yield farm on Binance Smart Chain.</Text>
+        <Text>BBT-Swap</Text>
+        <Text>The #1 AMM and yield farm on Bitkub Smart Chain.</Text>
       </BreadCrumbs>
     </Text>
   );
@@ -34,22 +34,22 @@ export const CustomSeparator: React.FC = () => {
     <Text p="32px">
       <Text mb="16px">
         <BreadCrumbs separator={<LogoIcon width="24px" />}>
-          <Link href="/" color="secondary" style={{ fontWeight: 400 }}>
+          <Link href="/" color="bbtColor" style={{ fontWeight: 400 }}>
             Link
           </Link>
-          <Text color="textDisabled">Crumb 1</Text>
-          <Text color="textDisabled">Crumb 2</Text>
+          <Text color="textBitkub">Crumb 1</Text>
+          <Text color="textBitkub">Crumb 2</Text>
         </BreadCrumbs>
       </Text>
       <Text mb="16px">
-        <BreadCrumbs separator={<PresentWonIcon width="48px" />}>
+        <BreadCrumbs separator={<KubIcon width="48px" />}>
           <Link href="/" color="failure" style={{ fontWeight: 400 }}>
             Link
           </Link>
           <Link href="/" color="primary" style={{ fontWeight: 400 }}>
             Link 2
           </Link>
-          <Text color="textDisabled">Crumb 2</Text>
+          <Text color="textBitkub">Crumb 2</Text>
         </BreadCrumbs>
       </Text>
     </Text>
